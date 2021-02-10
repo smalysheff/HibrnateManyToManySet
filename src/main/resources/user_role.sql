ced6ce66f286b9f4bc7c288a8ca54d5bdb9fe435
Sergey <malyshevs_v@mail.ru>
2021-02-10 22:35:03 +0300

    Sergey

---
+++ src/main/resources/user_role.sql
@@ -0,0 +1,10 @@
+insert into user_role (registr_date, role_id, user_id) values ('2021-02-10 22:21:58', 1, 1);
+insert into user_role (registr_date, role_id, user_id) values ('2021-02-10 22:21:58', 2, 1);
+insert into user_role (registr_date, role_id, user_id) values ('2021-04-10 22:21:58', 3, 2);
+insert into user_role (registr_date, role_id, user_id) values ('2021-05-10 22:21:58', 4, 2);
+insert into user_role (registr_date, role_id, user_id) values ('2021-06-10 22:21:58', 1, 3);
+insert into user_role (registr_date, role_id, user_id) values ('2021-07-10 22:21:58', 2, 3);
+insert into user_role (registr_date, role_id, user_id) values ('2021-08-10 22:21:58', 5, 4);
+insert into user_role (registr_date, role_id, user_id) values ('2021-09-10 22:21:58', 2, 5);
+insert into user_role (registr_date, role_id, user_id) values ('2021-10-10 22:21:58', 3, 6);
+insert into user_role (registr_date, role_id, user_id) values ('2021-11-10 22:21:58', 4, 6);
