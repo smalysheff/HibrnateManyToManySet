8f7d3b410197f28cda9d98b1b9536e7c75196383
Sergey <malyshevs_v@mail.ru>
2021-02-19 01:36:25 +0300

    changes

--- src/main/java/ru/sapteh/controller/MainController.java
+++ src/main/java/ru/sapteh/controller/MainController.java
@@ -4,8 +4,11 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.util.Callback;
 import org.hibernate.SessionFactory;
@@ -21,8 +24,8 @@ import java.util.Objects;
 
 public class MainController {
 
+    //TableView
     private final ObservableList<User> userList = FXCollections.observableArrayList();
-
     @FXML
     private TableView<User> userTableView;
     @FXML
@@ -38,6 +41,11 @@ public class MainController {
     @FXML
     private TableColumn<User, String> lastRegRoleColumn;
 
+    //Bottom
+    @FXML
+    private Label countLabel;
+
+
     @FXML
     public void initialize(){
 
@@ -46,29 +54,42 @@ public class MainController {
 
         idColumn.setCellValueFactory(u ->
                 new SimpleObjectProperty<>(u.getValue().getId()));
-
         lastNameColumn.setCellValueFactory(userStringCellDataFeatures ->
                 new SimpleObjectProperty<>(userStringCellDataFeatures.getValue().getLastName()));
 
+        firstNameColumn.setEditable(true);
+        firstNameColumn.setOnEditCommit(event -> {
+            event.getTableView().getItems().get(event.getTablePosition().getRow()).setName(event.getNewValue());
+        });
+
+//        firstNameColumn.getTableView().getItems().get(
+//                new EventHandler<TableColumn.CellEditEvent<User, String>>() {
+//                    @Override
+//                    public void handle(TableColumn.CellEditEvent<User, String> event) {
+//                        System.out.println(event.getTablePosition().getRow());
+//                    }
+//                });
+
         firstNameColumn.setCellValueFactory(p ->
                 new SimpleObjectProperty<>(p.getValue().getName()));
-
         regDateColumn.setCellValueFactory(u ->
                 new SimpleObjectProperty<>(
                         u.getValue().getUserRoles().stream()
                         .min(Comparator.comparing(UserRole::getRegistrationDate))
                         .get().getRegistrationDate()));
-
         countRoleColumn.setCellValueFactory(u ->
                 new SimpleObjectProperty<>(u.getValue().getUserRoles().size()));
-
 //        lastRegRoleColumn.setCellValueFactory(u ->
 //                new SimpleObjectProperty<>(
 //                        u.getValue().getUserRoles().stream().findFirst().get().getRole().toString()));
-
-
+        userTableView.setEditable(true);
         userTableView.setItems(userList);
+
+        //Count users to database
+        countLabel.setText(String.valueOf(userList.size()));
     }
+
+
 
 
     private void findByAllUserToDataBase(){
